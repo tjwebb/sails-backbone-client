9dcdb5dd50a79adfa509f8613024230faafff316
Travis Webb <me@traviswebb.com>
2014-11-24 17:12:04 -0500

    save url property on ns

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var _ = require('lodash');
 var Promise = require('bluebird');
 var parser = require('./lib/parser');
+var url = require('url');
 
 var anchorRules = _.transform(require('anchor/lib/match/rules'), function (rules, rule, name) {
   rules[name] = function (value) {
@@ -17,17 +18,12 @@ require('backbone-validation');
 module.exports = {
   /**
    * Return Promise that resolves the API
-   * @param url String [optional] the url of the backbonemodels REST endpoint
-   * @param ns  Object namespace object
+   * @param url - String the url of the backbonemodels REST endpoint
+   * @param ns  - Object namespace object
    */
   create: function (_url, ns) {
-    if (!_.isObject(ns)) {
-      ns = { };
-    }
-    if (_.isObject(_url)) {
-      ns = _url;
-      _url = null;
-    }
+    _.isObject(ns) || (ns = { });
+    ns.url = url.parse(_url);
 
     Backbone.Relational.showWarnings = false;
     Backbone.Relational.store.addModelScope(ns);
@@ -35,15 +31,11 @@ module.exports = {
     _.extend(Backbone.Validation.validators, anchorRules);
     _.extend(Backbone.Model.prototype, Backbone.Validation.mixin);
 
-    var ModelCollection = Backbone.Collection.extend({
-      url: function () {
-        return _url || this.document.url() + '/backbonemodel';
-      }
-    });
-    var collection = new ModelCollection();
+    var ModelCollection = Backbone.Collection.extend({ url: _url });
+    var models = new ModelCollection();
 
     return new Promise(function (resolve, reject) {
-      collection.fetch({
+      models.fetch({
         success: function (collection, response) {
           resolve(_.extend(ns, parser.parse(response, ns)));
         },
