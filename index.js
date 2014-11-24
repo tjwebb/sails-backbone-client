12441fb15088d6e8552ad42c46716605b4a355f9
Travis Webb <me@traviswebb.com>
2014-11-24 02:45:25 -0500

    cleanup
    - refactored if statements into the affirmative
    - added inline comments
    - added guard clauses for create()

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-var Backbone = global.Backbone || require('backbone');
+'use strict';
+
+var _ = require('lodash');
 var Promise = require('bluebird');
 var parser = require('./lib/parser');
+
+var Backbone = global.Backbone || (global.Backbone = require('backbone'));
+require('backbone-relational');
+require('backbone-validation');
 
 module.exports = {
   /**
    * Return Promise that resolves the API
+   * @param url String [optional] the url of the backbonemodels REST endpoint
+   * @param ns  Object namespace object
    */
   create: function (_url, ns) {
+    if (!_.isObject(ns)) {
+      ns = { };
+    }
+    if (_.isObject(_url)) {
+      ns = _url;
+      _url = null;
+    }
+
+    Backbone.Relational.showWarnings = false;
+
     var ModelCollection = Backbone.Collection.extend({
       url: function () {
         return _url || this.document.url() + '/backbonemodel';
@@ -17,10 +35,8 @@ module.exports = {
     return new Promise(function (resolve, reject) {
       collection.fetch({
         success: function (collection, response) {
-          resolve(parser.parse({
-            models: response,
-            version: '0'
-          }, ns));
+          Backbone.Relational.store.addModelScope(ns);
+          resolve(_.extend(ns, parser.parse(response, ns)));
         },
         error: function (collection, error) {
           reject(new Error(JSON.stringify(error)));
